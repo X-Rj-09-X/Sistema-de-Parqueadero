9f8f57c69f43eccf02abd0e8491cf7b28aa3d86e
X-Rj-09-X <rikardojfp@hotmail.com>
2026-05-17 03:41:58 -0500

    creacion de pack vista y clase ventana principal con su swing

--- src/Principal/Main.java
+++ src/Principal/Main.java
@@ -3,6 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package Principal;
+import vista.VentanaPrincipal;
 
 /**
  *
@@ -15,6 +16,9 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        new VentanaPrincipal();
+        
     }
     
 }
---
+++ src/vista/VentanaPrincipal.java
@@ -0,0 +1,99 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package vista;
+import controlador.ControladorParqueadero;
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ *
+ * @author Ricardo J
+ */
+public class VentanaPrincipal extends JFrame {
+    
+    private JPanel panel;
+    private JButton btnCliente;
+    private JButton btnAdmin;
+    private JButton btnContabilidad;
+    private JButton btnSalir;
+    private ControladorParqueadero controlador;
+
+   
+    public VentanaPrincipal() {
+
+        controlador = new ControladorParqueadero();
+
+        setTitle("Sistema Parqueadero");
+
+        setSize(500, 400);
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        setLocationRelativeTo(null);
+
+        iniciarComponentes();
+
+        setVisible(true);
+    }
+
+    //componentes
+    public void iniciarComponentes() {
+
+        panel = new JPanel();
+
+        panel.setLayout(null);
+
+        panel.setBackground(new Color(30, 30, 30));
+
+        this.add(panel);
+
+        JLabel titulo;
+
+        titulo = new JLabel("SISTEMA PARQUEADERO");
+
+        titulo.setBounds(80, 40, 350, 40);
+
+        titulo.setForeground(Color.WHITE);
+
+        titulo.setFont(new Font("Arial", Font.BOLD, 24));
+
+        panel.add(titulo);
+
+        //boton cliente
+        btnCliente = new JButton("CLIENTE");
+
+        btnCliente.setBounds(150, 100, 180, 40);
+
+        panel.add(btnCliente);
+
+        //boton admin
+        btnAdmin = new JButton("ADMINISTRADOR");
+
+        btnAdmin.setBounds(150, 160, 180, 40);
+
+        panel.add(btnAdmin);
+
+        //boton contabilidad
+        btnContabilidad = new JButton("CONTABILIDAD");
+
+        btnContabilidad.setBounds(150, 220, 180, 40);
+
+        panel.add(btnContabilidad);
+
+        //boton salir
+        btnSalir = new JButton("SALIR");
+
+        btnSalir.setBounds(150, 280, 180, 40);
+
+        panel.add(btnSalir);
+
+        //salir
+        btnSalir.addActionListener(e -> {
+
+            System.exit(0);
+        });
+    }
+    
+}
